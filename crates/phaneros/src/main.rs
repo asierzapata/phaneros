7c86a9aac1dd93bde057fb36e6fcfce5980beb13
asierzapata <asier.zapata@gmail.com>
2026-07-12 23:45:50 +0200

    working scanner v1

--- crates/phaneros/src/main.rs
+++ crates/phaneros/src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use phaneros::scanner::Scanner;
+
+fn main() {
+    let mut scanner = Scanner::new(String::from("/Users/asierzapata/.dotfiles"));
+
+    let scanner_events = scanner.events();
+
+    scanner_events.subscribe(phaneros::scanner::ScannerEvent::ScanStarted, |file_path| {
+        println!("Scan started for path: {}", file_path);
+    });
+
+    scanner_events.subscribe(
+        phaneros::scanner::ScannerEvent::ScanCompleted,
+        |file_path| {
+            println!("Scan completed for path: {}", file_path);
+        },
+    );
+
+    scanner.scan();
+}
---
+++ crates/phaneros/src/utils/observer.rs
@@ -0,0 +1,35 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+
+/// A subscriber (listener) has type of a callable function.
+pub type Subscriber = fn(file_path: &str);
+
+/// Publisher sends events to subscribers (listeners).
+#[derive(Debug, Default)]
+pub struct Publisher<E> {
+    events: HashMap<E, Vec<Subscriber>>,
+}
+
+impl<E: Eq + Hash> Publisher<E> {
+    pub fn subscribe(&mut self, event_type: E, listener: Subscriber) {
+        self.events.entry(event_type).or_default().push(listener);
+    }
+
+    pub fn unsubscribe(&mut self, event_type: &E, listener: Subscriber) {
+        if let Some(listeners) = self.events.get_mut(event_type) {
+            listeners.retain(|&x| !std::ptr::fn_addr_eq(x, listener));
+            if listeners.is_empty() {
+                self.events.remove(event_type);
+            }
+        }
+    }
+
+    // TODO: Make notify accept an arbitrary payload, file path is just a placeholder for now.
+    pub fn notify(&self, event_type: &E, file_path: &str) {
+        if let Some(listeners) = self.events.get(event_type) {
+            for listener in listeners {
+                listener(file_path);
+            }
+        }
+    }
+}
